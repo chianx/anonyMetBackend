d0c25218ec92671e43e044c9b7f7db37757b2b9f
Chintan Grover <chinxgrover@gmail.com>
2023-11-18 19:46:11 +0530

    this looks finr

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function createRooms() {
     }
 }
 
-const port = process.env.PORT || 4500;
+const port = process.env.PORT;
 server.listen(port, () => {
     console.log("Server is running on http://localhost:"+port);
     // createRooms();
